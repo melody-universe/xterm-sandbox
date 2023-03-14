9f40d89a0e486c5960b78ad785999abeb0b6f629
Melody Universe <melody.universe.2021@gmail.com>
2023-03-14 10:50:23 -0700

    added js extension

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FitAddon } from "xterm-addon-fit";
 import React, { useEffect, useRef } from "react";
 import { Terminal } from "xterm";
 import "xterm/css/xterm.css";
-import runGame from "./runGame";
+import runGame from "./runGame.js";
 
 export default () => {
   const div = useRef<HTMLDivElement>(null);
